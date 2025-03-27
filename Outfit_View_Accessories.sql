fb7ac0e08f9069824db6ea2e34481031dccac968
VD2017 <157435854+VD2017@users.noreply.github.com>
2025-03-27 16:13:17 -0400

    Delete Outfit_View_Accessories.sql

--- Outfit_View_Accessories.sql
+++
@@ -1,9 +0,0 @@
--- View with Outfit and ONLY Accessories
-#Creates a view of an outfit's accessories and ONLY it's accessories
-
-CREATE VIEW `Outfit_Accessories_View` AS
-SELECT idOutfit, id_User, Title, ImageURL
-FROM Outfit
-INNER JOIN With_Accessory ON Outfit.idOutfit = With_Accessory.Parent_Outfit_ID
-LEFT JOIN Clothing_Item ON Clothing_Item.idClothing_Item = With_Accessory.Accessory;
-
